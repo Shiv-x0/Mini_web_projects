f70821457c41f2790af46e1573e6d26fc9ffa638
Shiv-x0 <shivsalunke825@gmail.com>
2026-07-13 12:22:14 +0530

    kitty

---
+++ kitty/script.js
@@ -0,0 +1,14 @@
+//printing for variables 
+console.log("Hello world");
+
+// Variables 
+let message = "Hello duniya"
+
+let message;
+message = "YouTube"
+
+console.log("Message")
+
+// Dom :- Document object model  :- Strutural representation 
+
+document.getElementById("headingElement").textContent = "Technologies BY js";
